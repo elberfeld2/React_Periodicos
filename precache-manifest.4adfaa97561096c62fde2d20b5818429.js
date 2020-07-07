c89d3c2e320e87b91b2b6086612ae0a198f39ac3
elberfeld2 <elberfeld2@users.noreply.github.com>
2020-07-07 03:07:16 +0000

    deploy: f9b76a234c0bff1dd552ede07e15d4461869b4a0

---
+++ precache-manifest.4adfaa97561096c62fde2d20b5818429.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7c1be11179877ef2fc925d749f7e22",
+    "url": "/React_Periodicos/index.html"
+  },
+  {
+    "revision": "d9eb39b360dccfceb920",
+    "url": "/React_Periodicos/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b4d0a33e049cf4a28e5d",
+    "url": "/React_Periodicos/static/css/main.ff6611f7.chunk.css"
+  },
+  {
+    "revision": "d9eb39b360dccfceb920",
+    "url": "/React_Periodicos/static/js/2.05a4d3d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Periodicos/static/js/2.05a4d3d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d0a33e049cf4a28e5d",
+    "url": "/React_Periodicos/static/js/main.97fff9b7.chunk.js"
+  },
+  {
+    "revision": "adc9154369bc8a358490",
+    "url": "/React_Periodicos/static/js/runtime-main.2e4c2e0f.js"
+  },
+  {
+    "revision": "f16a1ad5b4b9c86c0f30cd484308cb3b",
+    "url": "/React_Periodicos/static/media/404.f16a1ad5.png"
+  },
+  {
+    "revision": "854842fa495e13de2d54968cec61b6d1",
+    "url": "/React_Periodicos/static/media/500.854842fa.png"
+  },
+  {
+    "revision": "eb728687589f75caecdb7f2b3d71c1d5",
+    "url": "/React_Periodicos/static/media/circles.eb728687.png"
+  },
+  {
+    "revision": "ea0b6d34fed7a01019a90e1b2f6729f6",
+    "url": "/React_Periodicos/static/media/periodico.ea0b6d34.png"
+  }
+]);
