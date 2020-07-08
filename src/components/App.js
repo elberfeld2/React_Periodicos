a0115d8bb472dbe5d3258ebf573aa8155749cb7d
Elberfeld Perez <enrique5001@hotmail.com>
2020-07-07 22:58:57 -0500

    Final

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,20 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ExercisesContainer from '../pages/ExercisesContainer'
 import ExerciseNewContainer from '../pages/ExerciseNewContainer'
-// import PeriodicosContainer from '../pages/PeriodicosContainer'
-// import PeriodicoContainer from '../pages/PeriodicoContainer'
+import PeriodicosContainer from '../pages/PeriodicosContainer'
+import PeriodicoContainer from '../pages/PeriodicoContainer'
 import NotFound from '../pages/404'
 
 const App = () => (
-    <BrowserRouter >
+    <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={ExercisesContainer} />
+            <Route exact path="/" component={PeriodicosContainer} />
+            <Route exact path="/exercise" component={ExercisesContainer} />
             <Route exact path="/exercise/new" component={ExerciseNewContainer} />
+            <Route exact path="/:id" component={PeriodicoContainer} />
             <Route component={NotFound}/>
         </Switch>
     </BrowserRouter>
 )
-/*
-            <Route exact path="/" component={PeriodicosContainer} />
-            <Route exact path="/:id" component={PeriodicoContainer} />
-*/
+
 export default App
