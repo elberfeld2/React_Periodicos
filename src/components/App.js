451184060b7cd1378b5c19467c7ecfa963ad716b
Elberfeld Perez <enrique5001@hotmail.com>
2020-07-07 22:50:13 -0500

    Comentarios 2

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ExercisesContainer from '../pages/ExercisesContainer'
 import ExerciseNewContainer from '../pages/ExerciseNewContainer'
-import PeriodicosContainer from '../pages/PeriodicosContainer'
-import PeriodicoContainer from '../pages/PeriodicoContainer'
+// import PeriodicosContainer from '../pages/PeriodicosContainer'
+// import PeriodicoContainer from '../pages/PeriodicoContainer'
 import NotFound from '../pages/404'
 
 const App = () => (
